fb8a4fe52e9bc93593f48dbd2c166c84d256cf7c
glumish <angga5597@gmail.com>
2020-07-24 15:36:08 +0700

    django-firstTry

---
+++ README.md
@@ -0,0 +1,1 @@
+# django-firstTry
---
+++ mysite/config.py
@@ -0,0 +1,9 @@
+ENGINE = 'django.db.backends.mysql'
+DB = 'django_test'
+USER = 'root'
+PASS = 'demimasadepan'
+HOST = 'localhost'
+
+STATIC_URL = '/static/'
+
+FORCE_SCRIPT_NAME = ''
